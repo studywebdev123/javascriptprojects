1f4d1853820a7c2022713bddf827744350fcc322
studywebdev123 <91095512+studywebdev123@users.noreply.github.com>
2022-01-26 13:21:57 -0800

    add projects

---
+++ Javascript Projects/Project2_functions/JS/mmain.js
@@ -0,0 +1,17 @@
+alert("goodbye world");
+document.write("this is a string");
+/*document.getElementById("header").innerHTML="changed text";
+function myFunction() {
+    document.getElementById("demo").innerHTML="Text";
+    
+}
+function displayDate() {
+    document.getElementById("demo").innerHTML=Date:;
+}*/
+//assignment 2
+function myFunctiona() {
+    var sentence = "I am learning ";
+    sentence += "a lot from this newspaper";
+    document.getElementById("Concatenate").innerHTML=sentence;
+    
+}
---
+++ Javascript Projects/main.js
@@ -0,0 +1,25 @@
+window.alert("this is a test")
+alert("This is another test");
+var x = "hello there";
+document.write(x);
+var a = "string";
+document.write(a);
+//assign a font color method
+var blues = "I have the blues";
+var blues = blues.fontcolor("Blue");
+document.write(blues);
+var C = "Hello to a new varable";
+window.alert(C);
+document.write("this is how" +"you" + "concatenate "+ "a string!");
+var B = "concatenated"+"string"
+document.write(B);
+document.write("this is an example, of\"using the backslash character\", inside a string");
+ //multiple varables
+var family = "The Jones", Mom="Ruth",Pop="John",Son="Bob",Daughter="Ann"; 
+ document.write(Daughter);
+ 
+  //write an expression
+  document.write(12/6 + 10);        
+      
+      
+   

165bed052fd7bffda48593e1e706f99aeac0c8fe
studywebdev123 <91095512+studywebdev123@users.noreply.github.com>
2022-02-02 13:43:24 -0800

    Project5

---
+++ Javascript Projects/Project2_functions/Project5/main.js
@@ -0,0 +1,31 @@
+alert("testing123");
+//assignment 26
+document.write(typeof"Math");
+document.write(typeof 3);
+document.write(typeof "word");
+//assignment
+function my_Function(){
+    document.getElementById("Test").innerHTML=0/0;
+}
+document.write(10>2);
+document.write(10<2);
+console.log(2 + 2);
+document.write("10"+ 5);
+document.write(10 + 5);
+console.log(10<2);
+document.write(10==10);
+document.write(3==10);
+//comparison operators
+document.write(10===10);
+document.write(10==="20");
+document.write(20==="20");
+document.write(10===20);
+//assignment33/logical operators
+document.write(5>2 &&10>4);
+document.write(5>2&&10<2);
+document.write(5>3||2>4);
+document.write(5<3||5<1);
+//not operator
+function not_Function()
+{document.getElementById("Not").innerHTML=!(20>10);
+}
